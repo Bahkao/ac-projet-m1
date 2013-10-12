16bf01605d9c41762b478a583772d0b9d1374e70
mbackend@gmail.com <mbackend@gmail.com@b14d4149-25e4-5a21-b889-f9c2885898a5>
2013-10-12 07:36:53 +0000

    Ajout menu

--- src/menu.c
+++ src/menu.c
@@ -9,7 +9,9 @@
 
 
 void menu_principale (){
-	
+	TypGraphe* graphe;
+	char chaine[3];
+	int tmp,choix,j;
 	
 	printf(  "##########################################\n" );
         printf(  "#             MENU PRINCIPALE            #\n" );
@@ -19,8 +21,86 @@ void menu_principale (){
 	printf(  "#	3 : Quitter                       #\n");
 	printf(  "##########################################\n" );
 	printf(  "Faites un choix\n");
+	scanf("%d",&choix);
+	switch (choix){
+		case 1 :
+			printf("Taper le nombre maximum de sommets:  \n");
+			scanf("%s",chaine);
+			sscanf(chaine,"%d",&tmp);
+			graphe = malloc(sizeof(TypGraphe));
+			j = creerGraphe(graphe, tmp);
+			break;
+		case 2 : 
+			exit(0);
+			break;
+	
+	}
+	sous_menu();
 	
 }
+
+/****************************************************
+ * Fonction   		:sous_menu
+ *
+ * Paramettre 		:rien
+ *
+ * Retour     		:rien
+ * 
+ * Description		: affichage du sous menu
+ ****************************************************
+ */
+void sous_menu(){
+	
+	int choix;
+	
+	printf(  "##########################################\n" );
+        printf(  "#                 SOUS MENU              #\n" );
+        printf(  "##########################################\n" );
+	printf(  "#	1 : Afficher le graphe            #\n");
+	printf(  "#	2 : Inserer un sommet             #\n");
+	printf(  "#	3 : Inserer une arête             #\n");
+	printf(	 "#	4 : Supprimer un sommet           #\n");
+	printf(	 "#	5 : Supprimer une arête           #\n");
+	printf(	 "#	6 : Sauvegarder le graphe         #\n");
+	printf(	 "#	7 : Charger le graphe             #\n");
+	printf(  "#	8 : Quitter                       #\n");
+	printf(  "##########################################\n" );
+	printf(" Faites un choix:\n ");
+	scanf("%d",&choix);
+	switch (choix){
+	case 1 :
+		printf("Afficher le graphe\n ");
+		
+	case 2 :
+		printf("Inserer un sommet\n");
+		printf("Saisir le numéro du sommet\n");
+		break;
+	case 3 :
+		printf("Inserer une arête \n");
+		printf( "Es une arête orientée ? \n 1-Oui 0-Non\n" );
+		break;
+	case 4 :
+		printf("Supprimer un sommet\n");
+		printf("Entrer le sommet à supprimer\n");
+		break;
+	case 5 :
+		printf("Supprimer une arête\n");
+		printf("Inserer le sommet de départ : \n");
+		break;
+	case 6 :
+		
+		printf("Sauvegarder le graphe\n");
+		break;
+           
+	case 8 :
+		
+		exit(0);
+		break;
+	
+	}
+	sous_menu();
+}
+
 
 
 
